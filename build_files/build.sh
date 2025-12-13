98317267d991fff3ad41a9244fa3c62570674893
Eugene Akukin <eugene.akukin@dataart.com>
2025-12-13 14:59:13 +0000

    custom vivaldi install

--- build_files/build.sh
+++ build_files/build.sh
@@ -16,9 +16,21 @@ dnf5 install -y mc btop openconnect NetworkManager-openconnect plasma-nm-opencon
 rpm --import https://packages.microsoft.com/keys/microsoft.asc && echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf5 check-update && dnf5 install code
 
-# Install latest Vivaldi browser
+# Install latest Vivaldi browser to /usr/local instead of /opt
 VIVALDI_LATEST=$(curl -s https://repo.vivaldi.com/stable/rpm/x86_64/ | grep -oP 'vivaldi-stable-[0-9.]+-[0-9]+\.x86_64\.rpm' | sort -V | tail -n1)
-dnf5 install --nogpgcheck -y "https://repo.vivaldi.com/stable/rpm/x86_64/${VIVALDI_LATEST}"
+cd /tmp
+curl -LO "https://repo.vivaldi.com/stable/rpm/x86_64/${VIVALDI_LATEST}"
+rpm2cpio "${VIVALDI_LATEST}" | cpio -idmv
+# Move from extracted /opt to /usr/local
+mkdir -p /usr/local
+mv opt/vivaldi /usr/local/
+# Install desktop files and icons to standard locations
+if [ -d usr/share ]; then
+    cp -r usr/share/* /usr/share/
+fi
+# Clean up
+rm -rf opt usr "${VIVALDI_LATEST}"
+cd -
 
 # Use a COPR Example:
 #
